36129a923dd52444bf83e42f58d65cf69bc0666e
ayang212 <ayang212@gmail.com>
2022-11-13 15:35:09 -0500

    load_files.py now outputs word vectors for training data into a json file

--- embeddings.py
+++ embeddings.py
@@ -1,7 +1,8 @@
 from gensim.models import FastText
 import os
-from load_files import tokenize_text, load_text, sentence_seg
+from load_files import tokenize_text, load_text, sentence_seg, generate_labels
 import xml.etree.ElementTree as ET
+import json
 
 
 def main():
@@ -10,15 +11,45 @@ def main():
     # path = r'.\CSE6250_project\CSE6250_projectdata\train\100.xml'
     path = r'.\CSE6250_projectdata\train'
     corpus = []
-    for filename in os.listdir(path):
+    labels_list = []
+    ft_model = FastText()
+
+    for i, filename in enumerate(os.listdir(path)):
         fullname = os.path.join(path, filename)
-        data = load_text(fullname)
+        data, labels_ET = load_text(fullname)
+        labels_dict = generate_labels(labels_ET)
+        
+        labels_list.append(labels_dict)
         tokens = sentence_seg(data)
         corpus.append(tokens)
-    
-    ft_model = FastText()
-    ft_model.build_vocab(corpus)
-    ft_model.train(corpus, total_examples=ft_model.corpus_count, epochs=10)
+
+        # build vocabulary of fasttext word embeddings
+        if i == 0:
+            ft_model.build_vocab(tokens)
+        else:
+            ft_model.build_vocab(tokens, update=True)
+        
+        ft_model.train(tokens, total_examples=ft_model.corpus_count, epochs=10)
+
+    file_output_list = []
+    for i, doc in enumerate(corpus):
+            
+        temp_dict = {}
+        temp_dict["id"] = i
+
+        doc_flat = [item for sublist in doc for item in sublist]
+        vec = ft_model.wv[doc_flat]
+        temp_dict["data"] = vec.tolist()
+        temp_dict["labels"] = labels_list[i]
+        file_output_list.append(temp_dict)
+    output_data = {}
+    output_data["training_data"] = file_output_list
+
+    # generate vector for each doc with labels
+    with open("training_data.json", "w") as outfile:
+        json.dump(output_data, outfile)
+                
+        
     ft_model.save(r'.trained_embeddings')
 
     # # use BioWordVec binary to create a gensim model for embeddings
--- load_files.py
+++ load_files.py
@@ -30,12 +30,13 @@ def tokenize_text(data):
             # only tokenize non-stop words and if length of the word is greater than 1
             if (word not in STOP_WORDS) and (len(word) > 1):
                 tokens.append(word)
-        doc_token_list.extend(tokens)
+        if tokens:
+            doc_token_list.extend(tokens)
     return doc_token_list
 
 def sentence_seg(data):
     # data: expecting XML item with text attribute
-    # sentence_seg() returns sent_token_list, a list of cleaned sentences from the input text data
+    # sentence_seg() returns sent_token_list, a list of lists where each nested list is a tokenized sentence from the input text data
 
     # remove extra white spaces and replace with single space
     text = " ".join(data.text.split())
@@ -61,7 +62,10 @@ def sentence_seg(data):
             # only tokenize non-stop words and if length of the word is greater than 1
             if (word not in STOP_WORDS) and (len(word) > 1):
                 tokens.append(word)
-        sent_token_list.append(' '.join(tokens))
+        
+        # only append if list is not empty
+        if tokens:
+            sent_token_list.append(tokens)
     return sent_token_list
 
 def load_text(path):
@@ -69,27 +73,32 @@ def load_text(path):
     
     # training text under the 'TEXT' tag
     data = root.find('TEXT')
-    return data
+    labels = root.find('TAGS')
 
-def main():
-    # take 100.xml as an initial example to parse
-    path = r'CSE6250_projectdata\train\100.xml'
+    return data, labels
 
-    data = load_text(path)
-    tokenize_text(data)
-
-
-    ##### process response criteria/variables and store results to dictionary 'responses_dict'
-    # response criteria under the 'TAGS' tag
-    responses_parent = root.find('TAGS')
+def generate_labels(labels):
     data_dict = {}
-    for child in responses_parent:
+    for child in labels:
         criteria = child.tag.lower()
         if child.attrib['met'] == 'met':
             val = 1
         else:
             val = 0
         data_dict[criteria] = val
+    return data_dict
+
+def main():
+    # take 100.xml as an initial example to parse
+    path = r'CSE6250_projectdata\train\100.xml'
+
+    data, labels = load_text(path)
+    tokenize_text(data)
+
+
+    ##### process response criteria/variables and store results to dictionary 'responses_dict'
+    # response criteria under the 'TAGS' tag
+
 
 if __name__ == "__main__":
     main()
